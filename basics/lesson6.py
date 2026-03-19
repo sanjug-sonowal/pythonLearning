298ef69d3a8f66a7cdb39a9d83a257bcf3180b38
Sanjug Sonowal <sanjugsonowalofficial@gmail.com>
2026-03-19 13:04:22 +0530

    add control flow if elif else and for loop and while loop

---
+++ basics/lesson6.py
@@ -0,0 +1,24 @@
+#control flow
+'''
+in python we do have some control flow machanism like
+if elif and else and then we have loops
+in loops we have for loop and while loop
+'''
+
+age = 17
+
+if(age >= 18):
+    print("can get driving licence")
+else:
+    print("cannot eligible for the driving licence")
+
+
+for i in range(age):
+    print("from for loop ",i)
+
+
+j = 10;
+
+while(j <= 20):
+    print("from while loop ",j)
+    j += 1
