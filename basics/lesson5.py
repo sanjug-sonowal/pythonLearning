ab1635657ecd794abff80e6b6fb88144f65f2c43
Sanjug Sonowal <sanjugsonowalofficial@gmail.com>
2026-03-19 12:17:45 +0530

    operators in python

---
+++ basics/lesson5.py
@@ -0,0 +1,30 @@
+#operators in python
+
+#arithmetic operators
+num1 = 10
+num2 = 30
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 // num2)
+print(num1 ** num2) # exponential means power eg : 2 ^ 3
+
+#comparision operators
+
+print(num1 == num2)
+print(num1 != num2)
+print(num1 <= num2)
+print(num1 >= num2)
+print(num1 < num2)
+print(num1 > num2)
+
+
+# logical operators
+print(num1 and num2)
+print(num1 or num2)
+
+# membership operator
+print(num1 in [3,4,5,6,7])
+print(num2 not in [3,4,5,2,1,33])
