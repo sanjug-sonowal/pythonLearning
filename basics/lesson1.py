bf18f5aebfb86536eca18ddbc27316bcc00eddb8
Sanjug Sonowal <sanjugsonowalofficial@gmail.com>
2026-03-19 11:16:46 +0530

    comments practice

--- basics/lesson1.py
+++ basics/lesson1.py
@@ -1,1 +1,6 @@
 import this
+
+'''
+This is an Zen of python means here you will see some text when you run this import this,
+This will fetch some data from this package which we imported.
+'''
---
+++ basics/lesson2.py
@@ -0,0 +1,6 @@
+#single line comment
+'''
+This is 
+multiline
+comment
+'''
