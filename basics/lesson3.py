7e5271876fe8f609e11bf220a99de2225565d556
Sanjug Sonowal <sanjugsonowalofficial@gmail.com>
2026-03-19 11:42:49 +0530

    variables and data types

---
+++ basics/lesson3.py
@@ -0,0 +1,20 @@
+#Introduction to variables and types
+
+name = "sanjug"
+age = 28
+rollno = 21
+marks = 80.4
+is_present = True
+
+print(type(name))
+print(type(age))
+print(type(rollno))
+print(type(marks))
+print(type(is_present))
+
+
+print(name)
+print(age)
+print(rollno)
+print(marks)
+print(is_present)
